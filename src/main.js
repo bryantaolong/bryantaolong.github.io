29cab69dbb5af419e0913123767e77ce934bf1c5
Bryan Long <bryantaolong@outlook.com>
2026-02-24 14:27:46 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import routes from './router'
 import './styles/simple.css'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
